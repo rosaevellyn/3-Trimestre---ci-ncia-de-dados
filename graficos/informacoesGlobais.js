54e7c4fce0236c25de7572e8fe756b34fa405e98
rosaevellyn <165039637+rosaevellyn@users.noreply.github.com>
2024-09-17 11:14:32 -0300

    .

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -5,21 +5,17 @@ async function vizualizarInformacoesGlobais() {
     const dados = await res.json()
     const pessoasConectadas = (dados.total_pessoas_conectadas / 1e9)
     const pessoasNoMundo = (dados.total_pessoas_mundo / 1e9)
-    
-    const paragrafo = document.createElement('p')
-    paragrafo.classList.add('graficos-container__texto')
-    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${pessoasNoMundo} bilhões</span> de pessoas e que aproximadamente <span>${pessoasConectadas} bilhões</span> estão conectadas em alguma rede social e passam em média <span>${dados.tempo_medio}</span> horas conectadas.`
-    
-    // código omitido
-    
+    const horas = parseInt(dados.tempo_medio)
+    const minutos = Math.round((dados.tempo_medio - horas) * 100)
+    const porcentagemConectada = ((pessoasConectadas / pessoasNoMundo ) * 100).toFixed(2)
 
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
-    paragrafo.innerHTML = `Você sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente ${dados.total_pessoas_conectadas} estão conectadas em alguma rede social e passam em média ${dados.tempo_medio} horas conectadas.`
-    
-    const container = document.getElementById(‘graficos-container’)
+    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${pessoasNoMundo} bilhões</span> de pessoas e que aproximadamente <span>${pessoasConectadas} bilhões</span> estão conectadas em alguma rede social e passam em média <span>${horas} horas</span> e <span>${minutos} minutos</span> conectadas.<br>Isso significa que aproximadamente <span>${porcentagemConectada}%</span> de pessoas estão conectadas em alguma rede social.`
+
+    const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo)
-  }
-  
-  vizualizarInformacoesGlobais()
-  
+}
+
+vizualizarInformacoesGlobais()
+
--- graficos/quantidadeUsuarios.js
+++ graficos/quantidadeUsuarios.js
@@ -1,16 +1,16 @@
 import { getCSS, tickConfig } from "./common.js"
 
-async function quantidadeUsuariosPorRede () {
-    const url = 'https://raw.githubusercontent.com/evellynyasmin/api/main/numero-usuario'
+async function quantidadeUsuariosPorRede() {
+    const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
     const res = await fetch(url)
     const dados = await res.json()
     const nomeDasRedes = Object.keys(dados)
-    const quantidadeUsuarios = Object.values(dados)
+    const quantidadeDeUsuarios = Object.values(dados)
 
     const data = [
         {
-            x: nomeDasRedes,
-            y: quantidadeUsuarios,
+            x: nomeDasRedes, 
+            y: quantidadeDeUsuarios, 
             type: 'bar',
             marker: {
                 color: getCSS('--primary-color')
@@ -20,9 +20,9 @@ async function quantidadeUsuariosPorRede () {
 
     const laytout = {
         plot_bgcolor: getCSS('--bg-color'),
-        paper_bgcolor: getCSS('--bgcolor'),
+        paper_bgcolor: getCSS('--bg-color'),
         title: {
-            text: 'Redes sociais com mais uuários',
+            text: 'Redes sociais com mais usuários',
             x: 0,
             font: {
                 color: getCSS('--primary-color'),
@@ -32,9 +32,28 @@ async function quantidadeUsuariosPorRede () {
         },
         xaxis: {
             tickfont: tickConfig,
-            title: 'Nome das redes',
-            font: {
-                color: getCSS('--secondary-colo')
+            title: {
+                text: 'Nome das redes',
+                font: {
+                    color: getCSS('--secondary-color')
+                }
+            }
+        },
+        yaxis: {
+            tickfont: tickConfig,
+            title: {
+                text: 'Bilhões de usuários ativos',
+                font: {
+                    color: getCSS('--secondary-color')
+                }
             }
         }
+    }
 
+    const grafico = document.createElement('div')
+    grafico.className = 'grafico'
+    document.getElementById('graficos-container').appendChild(grafico)
+    Plotly.newPlot(grafico, data, laytout)
+}
+
+quantidadeUsuariosPorRede()
